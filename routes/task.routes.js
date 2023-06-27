7ca8285f968009e93dd16ad712b6a6600dcb251b
Joao Faria <joaorafaria@gmail.com>
2023-06-27 11:13:30 +0100

    add routes

---
+++ routes/task.routes.js
@@ -0,0 +1,29 @@
+// routes/task.routes.js
+
+const router = require("express").Router();
+// const mongoose = require("mongoose");
+
+const Task = require("../models/Task.model");
+const Project = require("../models/Project.model");
+
+//  POST /api/tasks  -  Creates a new task
+router.post("/tasks", (req, res, next) => {
+  const { title, description, projectId } = req.body;
+
+  Task.create({ title, description, project: projectId })
+    .then((taskFromDB) => {
+      return Project.findByIdAndUpdate(projectId, {
+        $push: { tasks: taskFromDB._id },
+      });
+    })
+    .then((response) => res.status(201).json(response))
+    .catch((err) => {
+      console.log("error creating a new task", err);
+      res.status(500).json({
+        message: "error creating a new task",
+        error: err,
+      });
+    });
+});
+
+module.exports = router;
